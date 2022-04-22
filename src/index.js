fdf9a761e06b06a0a42cbb46bae5d3eb0c352360
MardonovD <mardonovdoston69@gmail.com>
2022-04-22 11:06:00 +0500

    render

--- src/index.js
+++ src/index.js
@@ -5,21 +5,56 @@ import ReactDOM from "react-dom";
 import Header from "./Header.js";
 import Search from "./Search.js";
 import Card from "./Card.js";
-const Index = () => {
-  return (
-    <div className="big__container">
-      <Header />
-      <Search />
-     <div className="card-flex">
-     <Card />
-     <Card />
-     <Card />
-     <Card />
-     <Card />
-     <Card />
-     </div>
-    </div>
-  );
-};
+
+// const Index = () => {
+//   return (
+//     <div className="big__container">
+//       <Header />
+//       <Search />
+//       <div className="card-flex">
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//       </div>
+//     </div>
+//   );
+// };
+
+// ReactDOM.render(<Index />, document.querySelector("#root"));
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="big__container">
+        <Header />
+        <Search />
+        <div className="card-flex">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Index;
 
 ReactDOM.render(<Index />, document.querySelector("#root"));
