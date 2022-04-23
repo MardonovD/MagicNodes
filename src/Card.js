abf5cf8ce1c06a26d38104cc7962e5d8b70a3c4e
MardonovD <mardonovdoston69@gmail.com>
2022-04-23 10:23:22 +0500

    finish

--- src/Card.js
+++ src/Card.js
@@ -18,18 +18,18 @@ class Card extends React.Component {
   saqlash(event) {
     let data = new Date().toLocaleDateString();
     let saveCardDiv = document.querySelector(".saveCardDiv");
-
+    let btnSave = document.querySelector(".btn-Save");
     let miqdor = document.querySelector(".miqdor");
+    let ochirish = document.querySelector(".ochirish");
     miqdor.textContent = data;
 
     saveCardDiv.classList.remove("hidden");
 
     saveCardDiv.textContent = this.state.textareaValue;
     let hiddenInp = document.querySelector("#input-Card");
+    ochirish.classList.add("hidden");
     hiddenInp.classList.add("hidden");
-
-
-    
+    btnSave.classList.add("hidden");
   }
 
   render() {
@@ -44,6 +44,7 @@ class Card extends React.Component {
           onChange={(event) => this.uzgartirish(event)}
         ></textarea>
         <div className="saveCardDiv hidden"></div>
+        <div className="icon"></div>
         <div className="xaracteristic">
           <p className="miqdor">
             {this.state.miqdor - this.state.textareaValue.length} Simvol
@@ -51,6 +52,7 @@ class Card extends React.Component {
           <button className="btn-Save" onClick={(event) => this.saqlash(event)}>
             Save
           </button>
+          <i className="cut icon  ochirish hidden" id="remove "></i>
         </div>
       </div>
     );
